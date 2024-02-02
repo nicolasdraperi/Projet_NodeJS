79a59cbeb318a096007f94eecd05697f0964a9b3
nicolasdraperi <nicolas.draperi2@gmail.com>
2024-02-02 09:33:32 +0100

    Page admin liste des jeux

--- routes/jeuxRoute.js
+++ routes/jeuxRoute.js
@@ -10,8 +10,8 @@ Route.get('/createAllTable', Jeux.creatTables)
 Route.post('/ajouterJeux', Jeux.addJeux)
 Route.put('/modifJeux/:id', Jeux.updateJeux);
 Route.delete('/supprJeux/:id', Jeux.deleteJeux);
-Route.post('/jeux', User.authenticator,Jeux.listeJeux);
-Route.post('/acheterJeux', Jeux.acheterJeux);
+Route.post('/jeux', Jeux.listeJeux);
+Route.post('/acheterJeux', User.authenticator,Jeux.acheterJeux);
 
 
 
